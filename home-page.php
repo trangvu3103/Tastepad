e605480b7b4e40883165ba78500553982010adf5
trang vu <trang.vu@vitopmedia.co>
2020-04-16 09:27:27 +0700

    add js folder + style + home-page

---
+++ home-page.php
@@ -0,0 +1,32 @@
+<?php  ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Tastepad</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+    <header>
+      <div class="logo">
+        Tastepad
+      </div>
+      <div class="search-bar-div">
+        <form class="search-bar" action="index.html" method="post">
+          <input type="text" name="" value="">
+          <img src="" alt=""> <!--this will add the search icon-->
+        </form>
+      </div>
+      <div class="login">
+        <button type="login" name="button">Login</button>
+      </div>
+    </header>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>
