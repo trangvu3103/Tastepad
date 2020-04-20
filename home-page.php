3b506aeca88934c7ec35214ad905eb359bbe7aa5
trangvu3103 <trangvungockhanh3103@gmail.com>
2020-04-20 08:10:07 +0700

    add header with style

--- home-page.php
+++ home-page.php
@@ -9,22 +9,35 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo&family=Manrope&display=swap" rel="stylesheet">
   </head>
   <body>
-    <header>
-      <div class="logo">
-        Tastepad
-      </div>
-      <div class="search-bar-div">
-        <form class="search-bar" action="index.html" method="post">
-          <input type="text" name="" value="">
-          <img src="" alt=""> <!--this will add the search icon-->
-        </form>
-      </div>
-      <div class="login">
-        <button type="login" name="button">Login</button>
-      </div>
-    </header>
+    <div class="wrapper">
+      <header>
+        <div class="lo-search">
+          <div class="logo">
+            Tastepad
+          </div>
+          <div class="search-bar-div">
+            <form class="search-bar" action="index.html" method="post">
+              <img class="icon" src="img/icon/icons8-search-192.png" alt="">
+              <input type="text" name="" value="" placeholder="I want to search for...">
+            </form>
+          </div>
+        </div>
+        <div class="login">
+          <button type="button" name="button" class="login-btn button">Login</button>
+        </div>
+      </header>
+
+
+
+
+
+
+
+
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
