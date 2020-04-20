fbe2385967146b7a4424ce9af2d13fb4c48ed8db
trangvu3103 <trangvungockhanh3103@gmail.com>
2020-04-20 08:21:17 +0700

    add header.php + footer.php

---
+++ include/footer.php
@@ -0,0 +1,21 @@
+<footer>
+  <div class="copy-right">
+    Tastepad @ 2020
+  </div>
+  <div class="wrap-foo">
+    <div class="">
+      About
+    </div>
+    <div class="">
+      Contact
+    </div>
+    <div class="">
+      Language
+    </div>
+  </div>
+</footer>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+</html>
