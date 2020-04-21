92795780d4a4a847b97c86bad8703633dfcb4a10
trangvu3103 <trangvungockhanh3103@gmail.com>
2020-04-22 03:35:55 +0700

    update homepage

--- home-page.php
+++ home-page.php
@@ -72,7 +72,18 @@
         </div>
       </div>
       <!-- recipe list -->
-      <?php include 'include\recipes.php'; ?>
+      <?php include 'include\contests.php'; ?>
+    </div>
+    <div class="page">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
     </div>
   </div>
 </section>
